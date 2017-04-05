4504fde81e18d21a63b68fb03039940015a7f467
Marián Opiela <Marin Opiela>
2017-04-05 11:37:30 +0200

    Počítanie normálového vektora z 3 bodov.

---
+++ ColorPoint3D.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NormalVectorEstimation
+{
+    class ColorPoint3D
+    {
+        private float x;
+        private float y;
+        private float z;
+        private int r;
+        private int g;
+        private int b;
+
+        public ColorPoint3D(float x, float y, float z, int r, int g, int b)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        public float X { get => x; set => x = value; }
+        public float Y { get => y; set => y = value; }
+        public float Z { get => z; set => z = value; }
+        public int R { get => r; set => r = value; }
+        public int G { get => g; set => g = value; }
+        public int B { get => b; set => b = value; }
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ", " + Z + "; " + R + ", " + G + ", " + B + ")";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            ColorPoint3D point = obj as ColorPoint3D;
+            if (point == null)
+            {
+                return false;
+            }
+
+            return (this.X == point.X)
+            && (this.Y == point.Y)
+            && (this.Z == point.Z)
+            && (this.R == point.R)
+            && (this.G == point.G)
+            && (this.B == point.B);
+        }
+
+        public override int GetHashCode()
+        {
+            int x = (int)this.X;
+            int y = (int)this.Y;
+            int z = (int)this.Z;
+            return x ^ y ^ z ^ r ^ g ^ b;
+        }
+
+        public static ColorPoint3D operator +(ColorPoint3D u, ColorPoint3D v)
+        {
+            return new ColorPoint3D(
+                u.X + v.X,
+                u.Y + v.Y,
+                u.Z + v.Z,
+                0, 0, 0
+            );
+        }
+
+        public static ColorPoint3D operator -(ColorPoint3D u, ColorPoint3D v)
+        {
+            return new ColorPoint3D(
+                u.X - v.X,
+                u.Y - v.Y,
+                u.Z - v.Z,
+                0, 0, 0
+            );
+        }
+
+        public static ColorPoint3D crossProduct(ColorPoint3D u, ColorPoint3D v)
+        {
+            return new ColorPoint3D(
+                u.Y * v.Z - u.Z * v.Y,
+                u.Z * v.X - u.X * v.Z,
+                u.X * v.Y - u.Y * v.X,
+                0, 0, 0
+            );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NormalVectorEstimation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorPoint3D a = new ColorPoint3D(0, 0, 0, 255, 0, 0);
+            ColorPoint3D b = new ColorPoint3D(1, 0, 0, 0, 255, 0);
+            ColorPoint3D c = new ColorPoint3D(0, 1, 0, 0, 0, 255);
+
+            ColorPoint3D u = b - a;
+            ColorPoint3D v = c - a;
+            Console.WriteLine(u);
+            Console.WriteLine(v);
+
+            ColorPoint3D n = ColorPoint3D.crossProduct(u, v);
+            Console.WriteLine("Normal: " + n);
+
+            Console.ReadLine();
+        }
+    }
+}
